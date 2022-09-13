be3cbbb83a812fabd708160569c42cdec0ed9af6
Chris <chris.white098@gmail.com>
2022-09-13 03:50:20 +0100

    got video stream working

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ import cv2
 import RPi.GPIO as gp
 import time
 import numpy as np
-from picamera.array import PiRGBarray
+from picamera.array import PiRGBArray
 from picamera import PiCamera
 
 
@@ -21,7 +21,7 @@ class TacTip:
         self.raw_capture = PiRGBArray(self.camera, size=(self.width, self.height))
         
         # Let camera warm up
-        time.sleep(0.1)
+        time.sleep(0.5)
         
         
     def stream(self):
@@ -31,19 +31,33 @@ class TacTip:
             # grab the np array of the image
             image = frame.array
             
+            # Resize to be smaller and more managebale
+            image = cv2.resize(image, (480, 360))
+            
+            # clear the stream before the next capture
+            self.raw_capture.truncate()
+            self.raw_capture.seek(0)
+            
             # show the frame
             cv2.imshow(self.name, image)
             key = cv2.waitKey(1)
             
-            # clear the stream before the next capture
-            self.raw_capture.truncate(0)
+
             
             # Break on press of q
             if key == ord('q'):
                 break
-
+        '''
+        self.camera.capture(self.raw_capture, format='bgr')
+        image = self.raw_capture.array
+        cv2.imshow('test', image)
+        cv2.waitKey(0)
+        '''
+        
+        
+        
 def main():
-    tactip = TacTip(240,360,60,'Tactip1')
+    tactip = TacTip(1920,1088,32,'Tactip1')
     tactip.stream()
 
     return 0
